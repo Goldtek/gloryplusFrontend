bb649adf910bf4a5b455116a7fc47f599cafab31
clintfidel <clintfidel@gmail.com>
2019-11-10 11:42:09 +0100

    fix conflicts

--- src/components/sermon/index.js
+++ src/components/sermon/index.js
@@ -12,6 +12,12 @@ import {
 } from "../../custom";
 
 class SermonComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      liveStream: 'Yes'
+    }
+  }
   render() {
     return (
       <Fragment>
@@ -21,6 +27,7 @@ class SermonComponent extends Component {
         </Helmet>
         <TopNav />
         <Header />
+<<<<<<< HEAD
         <PageInfo title="Sermon" bgPicture="url(img/sermon-info.jpg)" />
 
         <Sermon
@@ -31,6 +38,21 @@ class SermonComponent extends Component {
         Shake yourself off the dust of limitation and begin to do exploits."
         />
         <BottomNav />
+=======
+        <PageInfo title="Sermon" />
+        <h2 className='online-church' align="center">ONLINE CHURCH</h2>
+        <hr style={{ color: '#E8E8E8'}}/>
+        <div className='container'>
+          <div class="embed-responsive embed-responsive-16by9">
+              <iframe poster='http://site.com/img.jpg' title='live-stream' className="embed-responsive-item loop controls" src="https://mdbootstrap.com/img/video/Sail-Away.mp4" ></iframe>
+          </div>
+        
+        </div>
+        <Sermon title="Doing Big things for God" preacher="Rev Dubus Achufusi" content="The God we serve is big. He created and designed us to do big things.
+         His predetermined purpose for creating us was to accomplish great things on earth.God designed you for exploits.
+        Shake yourself off the dust of limitation and begin to do exploits." />
+        <hr />
+>>>>>>> add live stream
         <SermonList />
         <NewsLetter />
         <Footer />
--- src/custom/sermon/index.js
+++ src/custom/sermon/index.js
@@ -1,48 +1,51 @@
-import React from "react";
-
-const Sermon = ({ title, preacher, content, link }) => (
-  <section className="sermon-section spad">
-    <div className="section-title">
-      <span>Experience God's Presence</span>
-      <h2>Sermon Today</h2>
-    </div>
-    <div className="sermon-warp">
-      <div
-        className="sermon-left-bg set-bg"
-        style={{
-          backgroundImage: "url(img/sermon-bg.jpg)"
-        }}
-      ></div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-md-offset-6">
-            <div className="sermon-content">
-              <h2>{title}</h2>
-              <ul className="sermon-info">
-                <li>
-                  Sermon By: <span>{preacher}</span>
-                </li>
-              </ul>
-              <p>{content}</p>
-              <div className="icon-links">
-                <a href="/">
-                  <i className="ti-link"></i>
-                </a>
-                <a href="/">
-                  <i className="ti-zip"></i>
-                </a>
-                <a href="/">
-                  <i className="ti-headphone"></i>
-                </a>
-                <a href="/">
-                  <i className="ti-import"></i>
-                </a>
+import React, { Component } from "react";
+const Sermon = ({title, preacher, content, link }) => (
+      <section className="sermon-section spad">
+        <div className="section-title">
+          {/* <span>Experience God's Presence</span> */}
+          <h2>Latest Sermon</h2>
+        </div>
+        <div className="sermon-warp">
+          <div
+            className="sermon-left-bg set-bg"
+            style={{
+              backgroundImage: "url(img/sermon-bg.jpg)"
+            }}
+          ></div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-6">
+                <div className="sermon-content">
+                  <h2>{title}</h2>
+                  <ul className="sermon-info">
+                    <li>
+                      Sermon By: <span>{preacher}</span>
+                    </li>
+                  </ul>
+                  <p>
+                    {content}
+                  </p>
+                  <div className="icon-links">
+                    <a href="/">
+                      <i className="ti-link"></i>
+                    </a>
+                    <a href="/">
+                      <i className="ti-zip"></i>
+                    </a>
+                    <a href="/">
+                      <i className="ti-headphone"></i>
+                    </a>
+                    <a href="/">
+                      <i className="ti-import"></i>
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      {/* </div> */}
+    {/* // </div> */}
   </section>
 );
 
