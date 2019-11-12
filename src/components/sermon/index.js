477cb66bca62ccccd603b637dd51715323b125d6
clintfidel <clintfidel@gmail.com>
2019-11-12 14:42:25 +0100

    add livestream

--- src/components/sermon/index.js
+++ src/components/sermon/index.js
@@ -11,11 +11,15 @@ import {
   SermonList
 } from "../../custom";
 
+const styles = {
+  background: '#737373'
+}
+
 class SermonComponent extends Component {
   constructor() {
     super();
     this.state = {
-      liveStream: 'Yes'
+      liveStream: 'false',
     }
   }
   render() {
@@ -27,32 +31,40 @@ class SermonComponent extends Component {
         </Helmet>
         <TopNav />
         <Header />
-<<<<<<< HEAD
-        <PageInfo title="Sermon" bgPicture="url(img/sermon-info.jpg)" />
-
-        <Sermon
-          title="Doing Big things for God"
-          preacher="Rev Dubus Achufusi"
-          content="The God we serve is big. He created and designed us to do big things.
-         His predetermined purpose for creating us was to accomplish great things on earth.God designed you for exploits.
-        Shake yourself off the dust of limitation and begin to do exploits."
-        />
-        <BottomNav />
-=======
         <PageInfo title="Sermon" />
         <h2 className='online-church' align="center">ONLINE CHURCH</h2>
         <hr style={{ color: '#E8E8E8'}}/>
         <div className='container'>
-          <div class="embed-responsive embed-responsive-16by9">
-              <iframe poster='http://site.com/img.jpg' title='live-stream' className="embed-responsive-item loop controls" src="https://mdbootstrap.com/img/video/Sail-Away.mp4" ></iframe>
-          </div>
+          {
+            this.state.liveStream ? (
+              <div className="embed-responsive embed-responsive-16by9 vp-text-alert-wrapper">
+                <div className="vp-alert-text hidden"></div>
+                <div className="vp-alert-time">
+                  <div className="vp-live-start-time-title">
+                    This event is scheduled for
+                  </div>
+                  <div className="vp-live-start-time-body">
+                    November 17
+                  </div>
+                  <div className="vp-live-start-time-footer">at 9:00 AM</div>
+                </div>
+                
+              </div>
+            ) : (
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe poster='http://site.com/img.jpg' title='live-stream' className="embed-responsive-item loop controls" src="rtmps://live-api-s.facebook.com:443/rtmp/" ></iframe>
+              </div>
+            )
+          }
+          
+          
+          
         
         </div>
         <Sermon title="Doing Big things for God" preacher="Rev Dubus Achufusi" content="The God we serve is big. He created and designed us to do big things.
          His predetermined purpose for creating us was to accomplish great things on earth.God designed you for exploits.
         Shake yourself off the dust of limitation and begin to do exploits." />
         <hr />
->>>>>>> add live stream
         <SermonList />
         <NewsLetter />
         <Footer />
