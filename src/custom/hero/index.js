66996ca2ac591352162ea8587614de831bcadf3f
ielemson <ielemson@gmail.com>
2019-12-17 07:05:34 +0100

    changed index slider

--- src/custom/hero/index.js
+++ src/custom/hero/index.js
@@ -1,150 +1,79 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "./slider-animations.css";
 import "./hero.css";
-const imgUrls = [
-  "img/gpa.jpg",
-  "img/mainChurch.jpg",
-  "img/rev&mumSmile.jpg",
-  "img/rev&mumDance.jpg"
+
+// import { Link } from "react-router-dom";
+// import "./hero.css";
+// const imgUrls = [
+//   "img/gpa.jpg",
+//   "img/mainChurch.jpg",
+//   "img/rev&mumSmile.jpg",
+//   "img/rev&mumDance.jpg"
+// ];
+// const texts = [
+//   "WELCOME TO GLORYPLUS INTERNATIONAL",
+//   "A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE",
+//   `LIVING IN GOD'S GLORY`,
+//   "HIS GLORY IS MADE MANIFEST IN HIS PEOPLE"
+// ];
+
+const content = [
+  {
+    title: "WELCOME TO GLORYPLUS INTERNATIONAL",
+    description: "A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE",
+    button1: "JOIN US",
+    button2: "LIVESTREAM",
+    image: "img/gpa.jpg"
+  },
+  {
+    title: "LIVING IN GOD'S GLORY",
+    description: "HIS GLORY IS MADE MANIFEST IN HIS PEOPLE",
+    button1: "JOIN US",
+    button2: "LIVESTREAM",
+    image: "img/rev&mumSmile.jpg"
+  },
+  {
+    title: "A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE",
+    description: "HIS GLORY IS MADE MANIFEST IN HIS PEOPLE",
+    button1: "JOIN US",
+    button2: "LIVESTREAM",
+    image: "img/mainChurch.jpg"
+  },
+  {
+    title: "HIS GLORY IS MADE MANIFEST IN HIS PEOPLE",
+    description: "A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE",
+    button1: "JOIN US",
+    button2: "LIVESTREAM",
+    image: "img/rev&mumDance.jpg"
+  }
 ];
-const texts = [
-  "WELCOME TO GLORYPLUS INTERNATIONAL",
-  "A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE",
-  `LIVING IN GOD'S GLORY`,
-  "HIS GLORY IS MADE MANIFEST IN HIS PEOPLE"
-];
+
 class Hero extends Component {
   render() {
     return (
       <section className="hero-section">
         <div className="hero-content">
           <div className="hc-inner">
-            <div id="myCarousel" className="carousel slide">
-              {/* <!-- Indicators --> */}
-              <ol className="carousel-indicators">
-                <li
-                  data-target="#myCarousel"
-                  data-slide-to="0"
-                  className="active"
-                ></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-              </ol>
-
-              {/* <!-- Wrapper for Slides --> */}
-              <div className="carousel-inner">
-                <div className="item active">
-                  {/* <!-- Set the first background image using inline CSS below. --> */}
-                  <div
-                    className="fill"
-                    style={{
-                      backgroundImage: "url('img/gpa.jpg')"
-                    }}
-                  ></div>
-                  <div className="carousel-caption">
-                    <h2 className="animated fadeInLeft">
-                      GLORY PLUS INTERNATIONAL
-                    </h2>
-                    <p className="animated fadeInUp">
-                      A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE
-                    </p>
-                    <p className="animated fadeInUp">
-                      <a
-                        href="#"
-                        className="btn btn-transparent btn-rounded btn-large"
-                      >
-                        JOIN US
-                      </a>
-                      &nbsp;
-                      <a
-                        href="#"
-                        className="btn btn-transparent btn-rounded btn-large"
-                      >
-                        LIVE STREAM
-                      </a>
-                    </p>
+            <Slider autoplay={3000} className="slider-wrapper">
+              {content.map((item, index) => (
+                <div
+                  key={index}
+                  className="slider-content"
+                  style={{
+                    background: `url('${item.image}') no-repeat center center`
+                  }}
+                >
+                  <div className="inner">
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
+                    <button>{item.button1}</button>&nbsp;
+                    <button>{item.button2}</button>
                   </div>
                 </div>
-                <div className="item">
-                  {/* <!-- Set the second background image using inline CSS below. --> */}
-                  <div
-                    className="fill"
-                    style={{
-                      backgroundImage: "url('img/mainChurch.jpg')"
-                    }}
-                  ></div>
-                  <div className="carousel-caption">
-                    <h2 className="animated fadeInDown">
-                      LIVING IN GOD'S GLORY
-                    </h2>
-                    <p className="animated fadeInUp">
-                      HIS GLORY IS MADE MANIFEST IN HIS PEOPLE
-                    </p>
-                    <p className="animated fadeInUp">
-                      <a
-                        href="#"
-                        className="btn btn-transparent btn-rounded btn-large"
-                      >
-                        JOIN US &nbsp;
-                      </a>
-                      <a
-                        href="#"
-                        className="btn btn-transparent btn-rounded btn-large"
-                      >
-                        LIVESTREAM
-                      </a>
-                    </p>
-                  </div>
-                </div>
-                <div className="item">
-                  {/* <!-- Set the third background image using inline CSS below. --> */}
-                  <div
-                    className="fill"
-                    style={{
-                      backgroundImage: "url('img/rev&mumSmile.jpg')"
-                    }}
-                  ></div>
-                  <div className="carousel-caption">
-                    <h2 className="animated fadeInRight">
-                      HIS GLORY IS MADE MANIFEST IN HIS PEOPLE
-                    </h2>
-                    <p className="animated fadeInRight">
-                      A GLORIOUS PEOPLE WITH A GLORIOUS MANDATE
-                    </p>
-                    <p className="animated fadeInRight">
-                      <a
-                        href="#"
-                        className="btn btn-transparent btn-rounded btn-large"
-                      >
-                        JOIN US
-                      </a>
-                      <a
-                        href="#"
-                        className="btn btn-transparent btn-rounded btn-large"
-                      >
-                        LIVESTREAM
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              {/* <!-- Controls --> */}
-              <a
-                className="left carousel-control"
-                href="#myCarousel"
-                data-slide="prev"
-              >
-                <span className="icon-prev"></span>
-              </a>
-              <a
-                className="right carousel-control"
-                href="#myCarousel"
-                data-slide="next"
-              >
-                <span className="icon-next"></span>
-              </a>
-            </div>
+              ))}
+            </Slider>
           </div>
         </div>
       </section>
