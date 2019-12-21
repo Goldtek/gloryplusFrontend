6dd7d85c0dcb6bebcce00e28354143cc24e6a2ae
ielemson <ielemson@gmail.com>
2019-12-21 19:35:25 +0100

    made changes to login , registration page

--- src/custom/member-login/index.js
+++ src/custom/member-login/index.js
@@ -1,38 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./login.css";
+import serializeForm from "form-serialize";
 
-const MemberLogin = () => {
-  return (
-    <div className="container ">
-      <div className="col-md-12 col-sm-12 col-lg-12 mt-5 login-div">
-        <div className="section-title title-left">
-          <h2 align="center">LOGIN FORM</h2>
-        </div>
-        <form className="contact-form ">
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8">
-              {" "}
-              <div className="col-sm-12">
-                <input type="email" placeholder="Your Email" />
+class MemberLogin extends React.Component {
+  handleLogin = e => {
+    e.preventDefault();
+    const loginValues = serializeForm(e.target, { hash: true });
+    console.log(loginValues);
+  };
+  render() {
+    return (
+      <div className="container ">
+        <div className="col-md-12 col-sm-12 col-lg-12 mt-5 login-div">
+          <div className="container login-container">
+            <div className="row">
+              <div className="col-md-6 login-form-1">
+                <h3>
+                  <img
+                    src="/img/glory.png"
+                    className="img-logo"
+                    alt="gloryplus-logo"
+                  />
+                </h3>
+                <form>
+                  <div className="form-group"></div>
+                  <div className="form-group"></div>
+                  <div className="form-group"></div>
+                  <div className="form-group">
+                    <Link to="/register" className="ForgetPwd">
+                      Not yet a member ? click here to register
+                    </Link>
+                  </div>
+                </form>
               </div>
-              <div className="col-sm-12">
-                <input type="password" placeholder=" Your Password" />
-                <button type="submit" className="site-login-btn">
-                  Login
-                </button>
-                &nbsp;&nbsp;
-                <Link to="/register" className="site-btn">
-                  Register
-                </Link>
+
+              <div className="col-md-6 login-form-2">
+                <h3>Member Login</h3>
+                <form onSubmit={this.handleLogin}>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Your Email *"
+                      required
+                      name="email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Your Password *"
+                      required
+                      name="password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input type="submit" className="btnSubmit" value="Login" />
+                  </div>
+                  <div className="form-group">
+                    <Link to="#" className="ForgetPwd" value="Login">
+                      Forget Password?
+                    </Link>
+                  </div>
+                </form>
               </div>
             </div>
-            <div className="col-md-2"></div>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MemberLogin;
--- src/custom/member-register/index.js
+++ src/custom/member-register/index.js
@@ -1,48 +1,101 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import serializeForm from "form-serialize";
 
-const MembeRegister = () => {
-  return (
-    <div className="container">
-      <div className="col-md-12 col-sm-12 col-lg-12 mt-5 login-div">
-        <div className="section-title title-left">
-          <h2 align="center">REGISTER FORM</h2>
-        </div>
-        <form className="contact-form">
-          <div className="row">
-            <div className="col-sm-6">
-              <input type="text" placeholder="First Name" />
-            </div>
-            <div className="col-sm-6">
-              <input type="text" placeholder="Last Name" />
-            </div>
-            <div className="col-sm-6">
-              <input type="text" placeholder="Email" />
-            </div>
-            <div className="col-sm-6">
-              <input type="text" placeholder="Phone Number" />
-            </div>
-            <div className="col-sm-6">
-              <input type="password" placeholder="  Password" />
-            </div>
-            <div className="col-sm-6">
-              <input type="password" placeholder=" Confirm Password" />
-            </div>
+class MembeRegister extends React.Component {
+  handleRegister = e => {
+    e.preventDefault();
+    const regValues = serializeForm(e.target, { hash: true });
+    console.log(regValues);
+  };
+  render() {
+    return (
+      <div className="container ">
+        <div className="col-md-12 col-sm-12 col-lg-12 mt-5 login-div">
+          <div className="container login-container">
+            <div className="row">
+              <div className="col-md-6 login-form-1 reg">
+                <h3>
+                  <img
+                    src="/img/glory.png"
+                    className="img-logo"
+                    alt="gloryplus-logo"
+                  />
+                </h3>
+                <form>
+                  <div className="form-group"></div>
+                  <div className="form-group"></div>
+                  <div className="form-group"></div>
+                  <div className="form-group">
+                    <Link to="/login" className="ForgetPwd">
+                      Already a member ? Click here to login
+                    </Link>
+                    <h4>
+                      We are glad to have you here, please register and proceed
+                      to login page.
+                    </h4>
+                  </div>
+                </form>
+              </div>
 
-            <div className="col-sm-12">
-              <textarea placeholder="Your Contact Address..."></textarea>
-              <button type="submit" className="site-login-btn">
-                Register
-              </button>
-              &nbsp;&nbsp;
-              <a href="/login" className="site-btn">
-                Login
-              </a>
+              <div className="col-md-6 login-form-2">
+                <h3>Member Registration</h3>
+                <form onSubmit={this.handleRegister}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Your Full Name *"
+                      required
+                      name="fullname"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Your Email *"
+                      required
+                      name="email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Your Password *"
+                      required
+                      name="password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control"
+                      placeholder="Your Address *"
+                      required
+                      name="address"
+                    ></textarea>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="submit"
+                      className="btnSubmit"
+                      value="Register"
+                    />
+                  </div>
+                  <div className="form-group">
+                    {/* <Link to="#" className="ForgetPwd" value="Login">
+                    
+                  </Link> */}
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MembeRegister;
