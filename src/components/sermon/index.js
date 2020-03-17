9ffa06c19439bc3bfe9f69b50b08e0b011da28f0
Elemson Ifeanyi <ielemson@gmail.com>
2020-03-17 12:25:09 +0100

    applying stash

--- src/components/sermon/index.js
+++ src/components/sermon/index.js
@@ -39,7 +39,7 @@ class SermonComponent extends Component {
 
   render() {
     const { sermons, loading } = this.state; //destructuring
-    console.log(sermons);
+
     return (
       <Fragment>
         <Helmet>
