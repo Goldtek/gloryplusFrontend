ff42c3f17f267ea92abe72edfcdb33a7311b9dc7
goldtek <dike4mii4real@gmail.com>
2020-03-20 21:16:59 +0100

    revm

--- src/custom/sermon-card/index.js
+++ src/custom/sermon-card/index.js
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-
-import { ws , RAVE_PUBLIC_KEY } from '../../lib/constant';
 import { download } from '../../lib/util';
 
 import "./audioplayer.css";
